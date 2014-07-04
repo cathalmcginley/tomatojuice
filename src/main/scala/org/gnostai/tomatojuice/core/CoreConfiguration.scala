c1afa29dc55fea65a5e2c7ddc4cd9017d0e15572
Cathal Mc Ginley <cathal.magus@gnostai.org>
2014-07-04 18:30:17 +0100

    load config from file as well as classpath

---
+++ src/main/scala/org/gnostai/tomatojuice/core/CoreConfiguration.scala
@@ -0,0 +1,23 @@
+package org.gnostai.tomatojuice.core
+
+import java.io.File
+import com.typesafe.config.ConfigFactory
+
+trait CoreConfigurationModule {
+
+  lazy val userConfigFile: File = {
+     val homeDir = System.getProperty("user.home")
+     val configDir = new File(homeDir, ".config")
+     val tjConfigDir = new File(configDir, "tomatojuice")
+     new File(tjConfigDir, "application.conf")
+  }
+  
+  lazy val userConfig = ConfigFactory.parseFile(userConfigFile)
+  
+  lazy val classpathConfig = ConfigFactory.load()
+  
+  lazy val config = {
+    userConfig.withFallback(classpathConfig)
+  }
+  
+}
